82e4ea13b688a80b4e891816fd9c6c5cafcdaea2
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-09-04 16:45:13 -0300

    tp1 ejercicio 7 inciso b - implementacion de funcion que aplica filtro de escala de grises y lo muestra en el canvas

--- tp1/js/imageDrawing.js
+++ tp1/js/imageDrawing.js
@@ -10,19 +10,67 @@ document.addEventListener("DOMContentLoaded", function() {
     // Obtener contexto del lienzo
     let ctx = canvas.getContext("2d");
 
-    let imageData;
-    // Crea un objeto ImageData (Canvas 2D API)
-    // imageData = ctx.createImageData(WIDTH, HEIGHT);
-    
     // Crea un objeto Image (Elemento HTML)
     let imagen = new Image();
     imagen.src = "img/powder.jpg";
     imagen.addEventListener('load', function() {
         ctx.drawImage(this, 0, 0);
-        imageData = ctx.getImageData(0, 0, this.width, this.height);
+    })
+
+    // Agrega un EventListener de click de mouse al canvas con el filtro de grises
+    canvas.addEventListener('click', function() {
+        let imageData = ctx.getImageData(0, 0, this.width, this.height);
+        filtroGrises(imageData);
         ctx.putImageData(imageData, 0, 0);
     })
  
-    // Dibuja la imagen en pantalla
-    
+    // Función que aplica filtro de escala de grises
+    function filtroGrises(imageData) {
+        let r = 0;
+        let g = 0;
+        let b = 0;
+        let a = 255;
+        let gris = 0;
+
+        // Recorre el arreglo de pixeles y setea el color de la imagen
+        for (x = 0; x < WIDTH; x++) {
+            for (y = 0; y < HEIGHT; y++) {
+                r = getRed(imageData, x, y);
+                g = getGreen(imageData, x, y);
+                b = getBlue(imageData, x, y);
+
+                gris = (r + g + b) / 3;
+
+                r = gris;
+                g = gris;
+                b = gris;
+
+                setPixel(imageData, x, y, r, g, b, a);
+            }
+        }
+    }
+
+    function getRed(imageData, x, y) {
+        let index = (x + y * imageData.width) * 4;
+        return imageData.data[index + 0];
+    }
+
+    function getGreen(imageData, x, y) {
+        let index = (x + y * imageData.width) * 4;
+        return imageData.data[index + 1];
+    }
+
+    function getBlue(imageData, x, y) {
+        let index = (x + y * imageData.width) * 4;
+        return imageData.data[index + 2];
+    }
+
+    function setPixel(imageData, x, y, r, g, b, a) {
+        let index = (x + y * imageData.width) * 4;
+
+        imageData.data[index + 0] = r;
+        imageData.data[index + 1] = g;
+        imageData.data[index + 2] = b;
+        imageData.data[index + 3] = a;
+    }
 })
