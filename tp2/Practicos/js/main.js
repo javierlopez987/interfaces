35d7983708b11b4fd85a3a69e787161081dc3880
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-09-25 16:40:44 -0300

    demo creacion de fichas con dos avatar

--- tp2/Practicos/js/main.js
+++ tp2/Practicos/js/main.js
@@ -11,28 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
     canvas.height = HEIGHT;
     let ctx = canvas.getContext("2d");
     let tablero = new Tablero(0, 0, WIDTH, HEIGHT, 'white', ctx);
-
-    let figureFactory;
-
- /*    figureFactory = new SolidCircleFactory(FIGURE_SIZE/2, ctx, tablero);
-    createFigures(figureFactory, FIGURE_NUM);
-
-    figureFactory = new SolidRectangleFactory(FIGURE_SIZE, ctx, tablero);
-    createFigures(figureFactory, FIGURE_NUM);
-
-    figureFactory = new GradientCircleFactory(FIGURE_SIZE, ctx, tablero);
-    createFigures(figureFactory, FIGURE_NUM);
-
-    figureFactory = new GradientRectangleFactory(FIGURE_SIZE * 2, ctx, tablero);
-    createFigures(figureFactory, FIGURE_NUM);
-
-    function createFigures(figureFactory, figureNum) {
-        let figure;
-        for (let index = 0; index < figureNum; index++) {
-            figure = figureFactory.createFigure();
-            tablero.addFigure(figure);
-        }
-    } */
     
     let pathImages = [
         "img/powder.jpg",
@@ -43,17 +21,6 @@ document.addEventListener("DOMContentLoaded", function() {
         "img/juan_luis_guerra.jpg",
         "img/ricky_martin.jpg"
     ]
-
- /*    for (let index = 0; index < FIGURE_NUM; index++) {
-        let img = new Image();
-        img.src = pathImages[Util.getIntRdm(pathImages.length)];
-        img.addEventListener('load', function() {
-            let pos = Util.getPositionRdm(WIDTH, HEIGHT);
-            let imageRectangle = new ImageRectangle(pos.x, pos.y, FIGURE_SIZE, FIGURE_SIZE, this, ctx);
-            tablero.addFigure(imageRectangle);
-            tablero.drawFigures();
-        })
-    } */
 
     let indexRdm = Util.getIntRdm(pathImages.length);
         
@@ -76,9 +43,7 @@ document.addEventListener("DOMContentLoaded", function() {
             let circle = new ImageCircle(pos.x, pos.y, FIGURE_SIZE, this, ctx);
             tablero.addFigure(circle);
         }
-        console.log(tablero);
         tablero.drawFigures();
     }
     
-    //tablero.drawFigures();
 })
