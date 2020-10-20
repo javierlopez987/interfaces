e0f17b5db218ccdf20637410615f1434ef4f4d61
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-10-20 19:36:04 -0300

    se agregan eventos a la pagina calendar

---
+++ tp3/EntregaFinal/js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    //#region CALENDAR JS
+    let btnToOpen = document.querySelectorAll("button.closed");
+    btnToOpen.forEach(element => {
+        element.addEventListener("click", open);
+    });
+
+    function open(e) {
+        let content = e.target.parentElement.parentElement.nextElementSibling;
+        content.classList.toggle("opened");
+    }
+    //#endregion
+})
