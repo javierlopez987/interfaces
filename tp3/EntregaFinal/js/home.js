b86ea16ac481e3b87ea1664892084b7c06b193da
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-11-03 00:50:26 -0300

    home - slides final implementation & integration

--- tp3/EntregaFinal/js/home.js
+++ tp3/EntregaFinal/js/home.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     
-    document.addEventListener("scroll", doSth);
+    document.addEventListener("scroll", showIn);
     let wrappers = document.querySelectorAll(".flip-cards .left,.flip-cards .center,.flip-cards .right");
     
-    function doSth(e) {
+    function showIn() {
         let appearanceGate = 450;
-        // console.log(e);
-        // console.log(this.scrollingElement.scrollTop);
         wrappers.forEach(element => {
             if((element.offsetTop - window.scrollY) < appearanceGate && (element.offsetTop - window.scrollY) > 0) {
-                // console.log("Element Offset " + element.offsetTop);
                 element.classList.add("appear");
             } else {
                 element.classList.remove("appear");
@@ -20,11 +17,40 @@ document.addEventListener("DOMContentLoaded", function () {
         const HEIGHT_SCROLL_HIDDEN = 1000;
         if(window.scrollY > HEIGHT_SCROLL_HIDDEN) {
             scrollIcon.classList.add("invisible");
-            console.log(document.querySelector(".scroll"));
         } else {
             scrollIcon.classList.remove("invisible");
         }
     }
 
+    let sliderBackward = document.querySelector(".slider.left");
+    let sliderForward = document.querySelector(".slider.right");
 
+    sliderBackward.addEventListener("click", slideBack);
+    sliderForward.addEventListener("click", slideForward);
+
+    let slides = document.querySelectorAll(".slides .wrapper");
+    let slidesPointer = 0;
+
+    function slideBack() {
+        slides[slidesPointer].classList.remove("slideActive");
+        slidesPointer--;
+        console.log(slides);
+        console.log(slidesPointer);
+        if(slidesPointer < 0) {
+            slidesPointer = slides.length - 1;
+            console.log("entra el if")
+            console.log(slides[slidesPointer]);
+        }
+        slides[slidesPointer].classList.add("slideActive");
+    }
+
+    function slideForward() {
+        console.log(slides[slidesPointer]);
+        slides[slidesPointer].classList.remove("slideActive");
+        slidesPointer++;
+        if(slidesPointer >= slides.length) {
+            slidesPointer = 0;
+        }
+        slides[slidesPointer].classList.add("slideActive");
+    }
 })
