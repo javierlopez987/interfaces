2dbf527d3c44dc1d7daf44a1e13e682ecab35916
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-11-04 10:57:26 -0300

    bug fixed on comments form

--- tp3/EntregaFinal/js/form.js
+++ tp3/EntregaFinal/js/form.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    let inputs = document.querySelectorAll('.formulario__input');
+    let inputs = document.querySelectorAll('.formulario .input');
     inputs.forEach(input => {
         input.addEventListener("change", function() {
             if (this.value.length >= 1) {
