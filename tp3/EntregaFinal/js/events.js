b936f4ed0c1858f6c22a9d6b54487f116e2bbb84
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-12-03 17:29:25 -0300

    mejora seccion eventos

--- tp3/EntregaFinal/js/events.js
+++ tp3/EntregaFinal/js/events.js
@@ -1,30 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    //#region CALENDAR JS
-    let btnToOpen = document.querySelectorAll("button.closed");
-    btnToOpen.forEach(element => {
-        element.addEventListener("click", open);
-    });
-
-    function open() {
-        console.log(this);
-        this.classList.toggle("closed");
-        let content = this.parentElement.parentElement.nextElementSibling;
-        content.classList.toggle("opened");
-        let linkEvent = this.parentElement.parentElement.nextElementSibling.nextElementSibling;
-        linkEvent.classList.toggle("show");
-        linkEvent.firstElementChild.firstElementChild.innerHTML = "Ver Foto";
-        linkEvent.addEventListener("click", showEvent);
-        let btnQuitFoto = linkEvent.nextElementSibling.firstElementChild;
-        btnQuitFoto.addEventListener("click", hide);
-    }
-    
-    function showEvent() {
-        this.nextElementSibling.classList.toggle("show");
-    }
-
-    function hide() {
-        this.parentElement.classList.toggle("show");
-    }
-    //#endregion
+ 
 })
