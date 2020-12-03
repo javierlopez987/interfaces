c2556c9c2258f4ccdae8fb09143d048bba1b70e8
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-12-03 14:16:16 -0300

    animaciones en seccion comentarios

--- tp3/EntregaFinal/js/comments.js
+++ tp3/EntregaFinal/js/comments.js
@@ -1,13 +1,60 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    //#region Form inputs
     let inputs = document.querySelectorAll('.formulario .input');
     inputs.forEach(input => {
         input.addEventListener("change", function () {
             if (this.value.length >= 1) {
-                this.nextElementSibling.classList.add('fijar');
+                this.classList.add('validado');
+                input.classList.remove("no-validado");
             } else {
-                this.nextElementSibling.classList.remove('fijar');
+                this.classList.remove('validado');
             }
         })
     });
+    //#endregion
+
+    let character = document.querySelector(".character-selector");
+    let form = document.querySelector(".formulario");
+
+    //#region Animacion entrada de componentes
+    setTimeout(() => showIn(character), 3500);
+    setTimeout(() => showIn(form), 3500);
+
+    function showIn(elem) {
+        if(elem.classList.contains("enviado")) {
+            elem.classList.remove("enviado");
+        }
+        elem.classList.add("appear");
+    }
+    //#endregion
+
+    //#region Form submit button
+    let summit = document.querySelector(".submit");
+    summit.addEventListener("click", send);
+
+    function send(e) {
+        e.preventDefault();
+        let validatedInputs = 0;
+        inputs.forEach(input => {
+            if(input.value.length >= 1) {
+                validatedInputs++;
+            } else {
+                input.classList.remove("validado");
+                input.classList.add("no-validado");
+            }
+        });
+
+        if(validatedInputs == 3) {
+            this.parentElement.classList.add("enviado");
+            this.parentElement.classList.remove("appear");
+            console.log(this.parentElement.previousElementSibling);
+            this.parentElement.previousElementSibling.classList.add("comentable");
+            inputs.forEach(input => {
+                input.value = "";
+                input.classList.remove("validado");
+            });
+        }
+    }
+    //#endregion
 })
